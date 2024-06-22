b412b0f7a1afe63eeebc4ae8bb1ee714b2d0f0e6
kotovskovv <kotovskovv@bk.ru>
2024-06-22 16:47:15 +0300

    ДЗ вводный модуль

---
+++ main.py
@@ -0,0 +1,46 @@
+print('Решение задачи "Длина слова"')
+print(len('Urban university'))
+print('Решение задачи "Суммы и разности"')
+first = 5
+second = 10
+summ = (first+second)
+print(summ)
+third = 1.5
+four = 2.5
+summ1 = (third + four)
+print(int(summ1))
+print('2 Решение задачи "Суммы и разности"')
+first = 15
+second = 10
+diff = (first-second)
+print(int(diff))
+third = 1.5
+four = 2.5
+diff1 = (third - four)
+print(diff1)
+print('Решение задачи "Среднее арифметическое"')
+first = 30
+second = 40
+third = 50
+mean = (first+second+third)/3
+print(int(mean))
+print('Решение задачи "Простые строчки"')
+first_string = 'Понедельник'
+second_string = 'Вторник'
+print(first_string+', '+second_string)
+print('2 Решение задачи "Простые строчки"')
+first_string = 'Понедельник, '
+second_string = 'Вторник'
+print(first_string+second_string)
+print('Решение задачи "Сложная формула"')
+a = 4
+b = 6
+c = 8
+f = ((a*b)+(a*c))
+print(int((f**3)/2))
+print('2 Решение задачи "Сложная формула"')
+a = 1
+b = 3
+c = 2
+f = ((a*b)+(a*c))
+print((f**3)/2)
